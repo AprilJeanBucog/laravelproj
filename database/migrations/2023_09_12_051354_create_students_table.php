f65e395f16b4953789305636440f0f08b0c535cb
AprilJeanBucog <apriljeanbucog50@gmail.com>
2023-10-03 09:17:40 +0700

    added activity5

--- database/migrations/2023_09_12_051354_create_students_table.php
+++ database/migrations/2023_09_12_051354_create_students_table.php
@@ -9,20 +9,10 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function up(): void
-    {
-        Schema: :create('students', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
@@ -35,6 +25,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
+    public function down(): void
     {
         Schema::dropIfExists('students');
     }

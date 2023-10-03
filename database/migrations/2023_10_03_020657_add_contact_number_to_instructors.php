d141c2debc31fee29930b204db21acdedb79adb7
AprilJeanBucog <apriljeanbucog50@gmail.com>
2023-10-03 09:54:20 +0700

    added activity # 6

--- database/migrations/2023_10_03_020657_add_contact_number_to_instructors.php
+++ database/migrations/2023_10_03_020657_add_contact_number_to_instructors.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('instructors', function (Blueprint $table) {
-            //
+            $table->string('contact_number', 11)->after('name'):
         });
     }
 
